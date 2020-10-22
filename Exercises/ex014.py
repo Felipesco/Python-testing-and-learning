c174dc402e7c27d4ddd2182f317dfb1412c3c7f2
Felipe Santos <felipesantosdealmeida.2020@gmail.com>
2020-10-22 09:43:42 -0300

    09h da manhã-- dia 22-10

---
+++ Exercises/ex014.py
@@ -0,0 +1,11 @@
+celsius = float(input('Digite a temperatura em °C: '))
+
+fahrenheit = (celsius * 9/5) + 32
+print('A temperatura de {}°C é igual a {:.1f}°F.'.format(celsius, fahrenheit))
+
+if celsius <= 12:
+    print('QUE FRIOOOO!!!')
+elif celsius <= 25:
+    print('O clima tá bom')
+else:
+    print('TÁ QUENTE MAAISSS!!!')
--- Tests/test0004.py
+++ Tests/test0004.py
@@ -1,5 +1,6 @@
-n1 = int(input('Digite um Valor: '))
-n2 = int(input('Digite outro Valor: '))
+n1 = float(input('Digite um Valor: '))
+n2 = float(input('Digite outro Valor: '))
+
 soma = n1 + n2
 subt = n1 - n2
 mult = n1 * n2
@@ -7,8 +8,12 @@ divi = n1 / n2
 restodiv = n1 % n2
 potenciax = n1 ** n2
 potenciay = n2 ** n1
+raiz1 = n1 ** (1/2)
+raiz2 = n2 ** (1/2)
+
 print(' A soma dos Valores é: {}'.format(soma))
 print(' A subtração dos valores é: {}'.format(subt))
 print(' A divisão dos valores é {}, e o resto em números reais é: {}'.format(divi, restodiv))
-print(' A potência de base {} com o expoente {} é: {}'.format(n1, n2, potenciax), end=' >>>>')
-print(' O inverso (base: {}; expoente: {}) é: {}'.format(n2, n1, potenciay))
+print(' A potência de base {} com o expoente {} é: {:.3f}'.format(n1, n2, potenciax), end=' >>>>')
+print(' O inverso (base: {}; expoente: {}) é: {:.3f}'.format(n2, n1, potenciay))
+print(' A raiz de {} é igual a {:.3f}. E a raiz de {} é igual a {:.3f}.'.format(n1, raiz1, n2, raiz2))
