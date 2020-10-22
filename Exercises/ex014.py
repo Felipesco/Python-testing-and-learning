d48e183de49e574db61173697fafdf5f4785ea56
Felipe Santos <felipesantosdealmeida.2020@gmail.com>
2020-10-22 09:47:50 -0300

    09h da manhã-- dia 22-10

--- Exercises/ex014.py
+++ Exercises/ex014.py
@@ -1,6 +1,6 @@
 celsius = float(input('Digite a temperatura em °C: '))
 
-fahrenheit = (celsius * 9/5) + 32
+fahrenheit = ( (celsius * 9) / 5) + 32
 print('A temperatura de {}°C é igual a {:.1f}°F.'.format(celsius, fahrenheit))
 
 if celsius <= 12:
